5cd96f7cda353cf651f0bd1830885b4327e4b050
Javier Maroto <javier.maroto@uhispano.ac.cr>
2024-04-11 10:00:47 -0600

    Initial commit

---
+++ CountDownTimer/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace CountDownTimer
+{
+    partial class CountDownTimer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.topLabel = new System.Windows.Forms.Label();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.startButton = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.timerLabel = new System.Windows.Forms.Label();
+            this.plusButton = new System.Windows.Forms.Button();
+            this.minusButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.changeBackgroundButton = new System.Windows.Forms.Button();
+            this.customAmountButton = new System.Windows.Forms.Button();
+            this.customTimeTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // topLabel
+            // 
+            this.topLabel.BackColor = System.Drawing.Color.Black;
+            this.topLabel.Font = new System.Drawing.Font("Nirmala UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.topLabel.ForeColor = System.Drawing.Color.IndianRed;
+            this.topLabel.Location = new System.Drawing.Point(-1, -1);
+            this.topLabel.Name = "topLabel";
+            this.topLabel.Size = new System.Drawing.Size(802, 79);
+            this.topLabel.TabIndex = 0;
+            this.topLabel.Text = "Count Down Timer";
+            this.topLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // resetButton
+            // 
+            this.resetButton.BackColor = System.Drawing.Color.White;
+            this.resetButton.Font = new System.Drawing.Font("Nirmala UI", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resetButton.Location = new System.Drawing.Point(288, 81);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(99, 49);
+            this.resetButton.TabIndex = 2;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = false;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.BackColor = System.Drawing.Color.White;
+            this.stopButton.Font = new System.Drawing.Font("Nirmala UI", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stopButton.Location = new System.Drawing.Point(411, 81);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(99, 49);
+            this.stopButton.TabIndex = 3;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = false;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // startButton
+            // 
+            this.startButton.BackColor = System.Drawing.Color.White;
+            this.startButton.Font = new System.Drawing.Font("Nirmala UI", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(288, 136);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(99, 49);
+            this.startButton.TabIndex = 4;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timerLabel
+            // 
+            this.timerLabel.BackColor = System.Drawing.Color.Black;
+            this.timerLabel.Font = new System.Drawing.Font("Nirmala UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timerLabel.ForeColor = System.Drawing.Color.IndianRed;
+            this.timerLabel.Location = new System.Drawing.Point(-1, 276);
+            this.timerLabel.Name = "timerLabel";
+            this.timerLabel.Size = new System.Drawing.Size(802, 61);
+            this.timerLabel.TabIndex = 5;
+            this.timerLabel.Text = "60 Seconds";
+            this.timerLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // plusButton
+            // 
+            this.plusButton.BackColor = System.Drawing.Color.White;
+            this.plusButton.Font = new System.Drawing.Font("Nirmala UI", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.plusButton.Location = new System.Drawing.Point(288, 395);
+            this.plusButton.Name = "plusButton";
+            this.plusButton.Size = new System.Drawing.Size(99, 49);
+            this.plusButton.TabIndex = 6;
+            this.plusButton.Text = "+";
+            this.plusButton.UseVisualStyleBackColor = false;
+            this.plusButton.Click += new System.EventHandler(this.plusButton_Click);
+            // 
+            // minusButton
+            // 
+            this.minusButton.BackColor = System.Drawing.Color.White;
+            this.minusButton.Font = new System.Drawing.Font("Nirmala UI", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minusButton.Location = new System.Drawing.Point(411, 395);
+            this.minusButton.Name = "minusButton";
+            this.minusButton.Size = new System.Drawing.Size(99, 49);
+            this.minusButton.TabIndex = 7;
+            this.minusButton.Text = "-";
+            this.minusButton.UseVisualStyleBackColor = false;
+            this.minusButton.Click += new System.EventHandler(this.minusButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.BackColor = System.Drawing.Color.White;
+            this.exitButton.Font = new System.Drawing.Font("Nirmala UI", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.Location = new System.Drawing.Point(411, 136);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(99, 49);
+            this.exitButton.TabIndex = 8;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = false;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // changeBackgroundButton
+            // 
+            this.changeBackgroundButton.BackColor = System.Drawing.Color.White;
+            this.changeBackgroundButton.Font = new System.Drawing.Font("Nirmala UI", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.changeBackgroundButton.Location = new System.Drawing.Point(288, 340);
+            this.changeBackgroundButton.Name = "changeBackgroundButton";
+            this.changeBackgroundButton.Size = new System.Drawing.Size(222, 49);
+            this.changeBackgroundButton.TabIndex = 9;
+            this.changeBackgroundButton.Text = "Change Background";
+            this.changeBackgroundButton.UseVisualStyleBackColor = false;
+            this.changeBackgroundButton.Click += new System.EventHandler(this.changeBackgroundButton_Click);
+            // 
+            // customAmountButton
+            // 
+            this.customAmountButton.BackColor = System.Drawing.Color.White;
+            this.customAmountButton.Font = new System.Drawing.Font("Nirmala UI", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.customAmountButton.Location = new System.Drawing.Point(288, 191);
+            this.customAmountButton.Name = "customAmountButton";
+            this.customAmountButton.Size = new System.Drawing.Size(222, 49);
+            this.customAmountButton.TabIndex = 10;
+            this.customAmountButton.Text = "Enter Custom Amount";
+            this.customAmountButton.UseVisualStyleBackColor = false;
+            this.customAmountButton.Click += new System.EventHandler(this.customAmountButton_Click);
+            // 
+            // customTimeTextBox
+            // 
+            this.customTimeTextBox.Location = new System.Drawing.Point(288, 247);
+            this.customTimeTextBox.Name = "customTimeTextBox";
+            this.customTimeTextBox.Size = new System.Drawing.Size(222, 26);
+            this.customTimeTextBox.TabIndex = 11;
+            // 
+            // CountDownTimer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.customTimeTextBox);
+            this.Controls.Add(this.customAmountButton);
+            this.Controls.Add(this.changeBackgroundButton);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.minusButton);
+            this.Controls.Add(this.plusButton);
+            this.Controls.Add(this.timerLabel);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.topLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "CountDownTimer";
+            this.Text = "CountDownTimer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label topLabel;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label timerLabel;
+        private System.Windows.Forms.Button plusButton;
+        private System.Windows.Forms.Button minusButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button changeBackgroundButton;
+        private System.Windows.Forms.Button customAmountButton;
+        private System.Windows.Forms.TextBox customTimeTextBox;
+    }
+}
+
---
+++ CountDownTimer/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CountDownTimer
+{
+    public partial class CountDownTimer : Form
+    {
+        public CountDownTimer()
+        {
+            InitializeComponent();
+        }
+
+        int timeleft = 60;
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(timeleft > 0)
+            {
+                timeleft = timeleft - 1;
+                timerLabel.Text = timeleft + " Seconds";
+               
+            }
+            else
+            {
+                timer.Stop();
+                timerLabel.Text = "Time is Up";
+            }
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            timer.Start();
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+            timeleft = 60;
+            timerLabel.Text = timeleft + " Seconds";
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+        }
+
+        private void plusButton_Click(object sender, EventArgs e)
+        {
+            timeleft = timeleft + 5;
+            timerLabel.Text = timeleft.ToString() + " Seconds";
+        }
+
+        private void minusButton_Click(object sender, EventArgs e)
+        {
+            timeleft = timeleft - 5;
+            timerLabel.Text = timeleft.ToString() + " Seconds";
+
+        }
+        private bool isFirstImage = true; // Start with the first image by default
+        private bool isSecondImage = false; // Flag to track current background image
+
+        private void changeBackgroundButton_Click(object sender, EventArgs e)
+        {
+            if (isFirstImage)
+            {
+                // Set background image to the second image
+                this.BackgroundImage = new Bitmap(@"C:\Users\mjavih\Desktop\Codes\csharp project\CountDownTimer\Background1.png");
+                isFirstImage = false;
+                isSecondImage = true;
+            }
+            else if (isSecondImage)
+            {
+                // Set background image back to default
+                this.BackgroundImage = null;
+                isSecondImage = false;
+            }
+            else
+            {
+                // Set background image to the first image
+                this.BackgroundImage = new Bitmap(@"C:\Users\mjavih\Desktop\Codes\csharp project\CountDownTimer\Background.png");
+                isFirstImage = true;
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void customAmountButton_Click(object sender, EventArgs e)
+        {
+            int customTime;
+
+            // Get user input from the customTimeTextBox control
+            string userInput = customTimeTextBox.Text;
+
+            // Try parsing the input to an integer representing seconds
+            if (int.TryParse(userInput, out customTime) && customTime > 0)
+            {
+                // Valid input - update timer and label
+                timeleft = customTime;
+                timerLabel.Text = customTime.ToString() + " Seconds";
+            }
+            else
+            {
+                // Invalid input - show an error message
+                MessageBox.Show("Please enter a valid starting time in seconds (positive number).");
+            }
+        }
+    }
+}
